a88c952ab59ce686d6cafbfc0d1b8246c761c502
Jungyeon Sohn <sjy20131565@gmail.com>
2019-06-06 17:57:03 +0900

    Connect gui and core

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from node.client import NodeClient
-
+from gui.guiNode import WindowNode
 
 def cli_loop(node_client):
     while True:
@@ -36,23 +36,6 @@ def cli_loop(node_client):
         else:
             pass
 
-
-if __name__ == "__main__":
-    argc = len(sys.argv)
-    is_miner = sys.argv[1] == 'miner' if argc > 1 else False
-    if argc < 2:
-        print('# Usage')
-        print('$ main.py <"miner" | ELSE> [ <name> [ <host>:<port> ]]')
-    name = sys.argv[2] if argc > 2 else 'unnamed'
-    host = sys.argv[3] if argc > 3 else 'localhost'
-    port = int(sys.argv[4]) if argc > 4 else 0
-
-    node_client = NodeClient(name, host, port, is_miner)
-    node_client.start()
-
-    cli_loop(node_client)
-
-
 class GuiHelper:
     
     def __init__(self, node_client):
@@ -83,3 +66,31 @@ class GuiHelper:
         if not node_client.is_miner: return
         pass
 
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    is_miner = sys.argv[1] == 'miner' if argc > 1 else False
+    if argc < 2:
+        print('# Usage')
+        print('$ main.py <"miner" | ELSE> [ <name> [ <host>:<port> ]]')
+    name = sys.argv[2] if argc > 2 else 'unnamed'
+    host = sys.argv[3] if argc > 3 else 'localhost'
+    port = int(sys.argv[4]) if argc > 4 else 0
+
+    node_client = NodeClient(name, host, port, is_miner)
+    node_client.start()
+    
+    #make gui parameters
+    gui_helper = GuiHelper(node_client)
+    my_addr = node_client.server_host + ":" + str(node_client.server_port)
+    gen_func = gui_helper.tx
+    if is_miner:
+        gen_func = gui_helper.blk
+    
+    #tmp
+    tmp = []
+    tmp2 = []
+
+    wnd_node = WindowNode(sys.argv[1], node_client.shorthand_id, my_addr, gui_helper.join, gui_helper.quit, gen_func, tmp, tmp2)
+    wnd_node.show()
+
+    #cli_loop(node_client)
