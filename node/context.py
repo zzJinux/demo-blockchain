651cf42dbd0804fe6b2d06b4b8ef84d05e5d9b12
Jinwook Jeong <vustthat@gmail.com>
2019-06-06 22:35:26 +0900

    add: DIFFICULTY_TARGET

--- node/context.py
+++ node/context.py
@@ -1,2 +1,6 @@
+from .toolbox import bytes_leading_zero
+
 AUTO_BLOCK_GEN = False
 TXS_PER_BLOCK = 10
+
+DIFFICULTY_TARGET = bytes_leading_zero(bytes_len = 256//8, n_zero_bits = 24)
--- node/toolbox.py
+++ node/toolbox.py
@@ -1,3 +1,17 @@
 def bytes_to_hexstring(b):
     return ''.join('%02x' % x for x in b)
 
+
+def bytes_leading_zero(bytes_len, n_zero_bits):
+    barr = bytearray(bytes_len)
+    n_skip = n_zero_bits // 8
+    n_partial = n_zero_bits % 8
+    
+    if n_partial > 0:
+        barr[n_partial] = (0b1 << (8 - n_partial)) - 1
+        n_skip += 1
+    
+    for i in range(n_skip, bytes_len):
+        barr[i] = 0xff
+
+    return bytes(barr)
