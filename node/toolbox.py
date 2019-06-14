01a0af00b0569eb40acd50770ef574b9495cc544
Jinwook Jeong <vustthat@gmail.com>
2019-06-14 13:26:33 +0900

    universal log

--- node/toolbox.py
+++ node/toolbox.py
@@ -1,3 +1,5 @@
+import requests
+
 def bytes_to_hexstring(b):
     return ''.join('%02x' % x for x in b)
 
@@ -15,3 +17,7 @@ def bytes_leading_zero(bytes_len, n_zero_bits):
         barr[i] = 0xff
 
     return bytes(barr)
+
+def mylog(text):
+    print(text)
+    # requests.post(url='http://localhost:8080', data=text)
